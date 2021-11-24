64daf01d504c91bcfc7bf6c13842caec8304a599
Max Sudik <maxsudik@gmail.com>
2021-11-24 08:58:35 -0800

    Adding lesson 5 regarding generics

---
+++ CodeGymTasks/4.JavaCollections/src/com/codegym/task/task35/task3502/Solution.java
@@ -0,0 +1,22 @@
+package com.codegym.task.task35.task3502;
+
+/* 
+Introducing generics
+
+*/
+
+import java.util.List;
+
+/* 
+Introducing generics
+
+*/
+
+public class Solution<T extends List<Solution.SomeClass>> {
+    public static class SomeClass<K extends Number> {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
